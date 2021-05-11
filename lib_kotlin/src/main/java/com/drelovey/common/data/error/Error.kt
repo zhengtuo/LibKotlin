3c81d979d12410d38fee8f1d3f428b58d3c5658e
zhengtuo <1171337677@qq.com>
2021-05-11 17:22:45 +0800

    error添加

--- lib_kotlin/src/main/java/com/drelovey/common/data/error/Error.kt
+++ lib_kotlin/src/main/java/com/drelovey/common/data/error/Error.kt
@@ -14,6 +14,7 @@ open class Error(val code: Int, val description: String) {
     companion object {
         const val NO_INTERNET_CONNECTION = 1
         const val UN_KNOW = -1
+        const val HAVE_MESSAGE = -2
         const val EMPTY = 404
     }
 }
